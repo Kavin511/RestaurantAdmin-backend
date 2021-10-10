cf5c7c2663f15c711be7501ab1c5cb0b9bd6c990
Kavin S <kavinalmighty@gmail.com>
2021-10-10 21:27:29 +0530

    db fix

--- checkToken.js
+++
@@ -1,6 +0,0 @@
-const jwt=require('jwt-simple')
-const config=require('./config')
-let checkToken=(req,res,next)=>{
-    let token=req.headers['authorization']
-    
-}
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const dbConfig = require('./dbconfig');
+const connectDb = async () => {
+    try {
+
+        const conn = await mongoose.connect(dbConfig.database,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log(`mongodb connected:${conn.connection.host}`);
+    } catch (error) {
+        console.log('error');
+        process.exit(1)
+    }
+}
+module.exports = connectDb
